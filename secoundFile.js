65a48693089aaf4310f910fc4ef912e4dfc26780
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-08-16 21:44:28 +0530

    edit ax

--- secoundFile.js
+++ secoundFile.js
@@ -3,20 +3,20 @@ class Solution {
    * @param {number[]} prices
    * @return {number}
    */
-  max_numProfit(prices) {
+  maxProfit(prices) {
     let min_so_far = 10000;
-    let max_num = 0;
+    let max = 0;
     let i = 0;
     while (i < prices.length) {
       if (prices[i] < min_so_far) {
         min_so_far = prices[i];
       }
       let newValue = prices[i] - min_so_far;
-      if (newValue > max_num) {
-        max_num = newValue;
+      if (newValue > max) {
+        max = newValue;
       }
       i++;
     }
-    return max_num;
+    return max;
   }
 }
