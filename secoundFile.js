f91e7b9f50f67bb931944fbd1343ad9cff7b2c1a
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-01-10 18:08:19 +0530

    promise example

--- secoundFile.js
+++ secoundFile.js
@@ -1,22 +1,27 @@
-class Solution {
-  /**
-   * @param {number[]} prices
-   * @return {number}
-   */
-  num_maxProfit(prices) {
-    let min_so_far = 10000;
-    let num_max = 0;
-    let i = 0;
-    while (i < prices.length) {
-      if (prices[i] < min_so_far) {
-        min_so_far = prices[i];
-      }
-      let newValue = prices[i] - min_so_far;
-      if (newValue > num_max) {
-        num_max = newValue;
-      }
-      i++;
-    }
-    return num_max;
-  }
-}
+const d= Promise.resolve("foo")
+  .then(
+    (string) => 
+      new Promise((resolve, reject) => {
+        setTimeout(() => {
+          string += "bar";       
+          resolve(string);   
+        }, 10000);
+      })
+  )
+console.log(d)
+  d.then((string) => {        
+    setTimeout(() => {
+      string += "baz";           
+      console.log(string);
+    },2000);
+    return string;         
+  })
+
+  .then((string) => {   
+    //1   
+    console.log(
+     "Last Then: oops... didn't bother to instantiate and return a promise in the prior then so the sequence may be a bit surprising"
+    );
+
+    console.log(string);          //2)foobar
+  });
