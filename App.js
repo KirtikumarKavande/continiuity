aa0fc779ef1f8f74612a27a1fae02a5ae6f79ee6
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-02-09 20:07:11 +0530

    promise tricky question

--- App.js
+++ App.js
@@ -1,7 +1,35 @@
-const App = () => {
-  console.log("good");
-};
+function abc(){
+  setTimeout(() => {
+   console.log("hi")
+   
+  }, 10000);
+}
 
-const root = document.getElementById("root");
-const rootNode = ReactDOM.createRoot(root);
-rootNode.render(App());
+Promise.resolve("foo")
+  .then(
+    (string) =>    //foo
+      new Promise((resolve, reject) => {
+        setTimeout(() => {
+          string += "bar";
+          resolve(string);
+        }, 1000);
+      })
+  )
+
+  .then((string) => {
+    setTimeout(() => {
+      string += "baz";
+      console.log(string);
+    }, 1);
+    return string;
+  })
+
+  .then((string) => {
+    console.log(
+      "Last Then: oops... didn't bother to instantiate and return a promise in the prior then so the sequence may be a bit surprising"
+    );
+
+    console.log("last",string);
+  });
+
+  console.log("good to go")
