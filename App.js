3f3783f1aa0a832ec13affc0c44e5d59b5094e86
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-02-15 19:21:03 +0530

    wave form 2D array

--- App.js
+++ App.js
@@ -1,35 +1,44 @@
-function abc(){
-  setTimeout(() => {
-   console.log("hi")
-   
-  }, 10000);
-}
+// function createCounter() {
+//   let count = 0;
 
-Promise.resolve("foo")
-  .then(
-    (string) =>    //foo
-      new Promise((resolve, reject) => {
-        setTimeout(() => {
-          string += "bar";
-          resolve(string);
-        }, 1000);
-      })
-  )
+//   function increment() {
+//     count++;
+//     console.log(count);
+//   }
 
-  .then((string) => {
-    setTimeout(() => {
-      string += "baz";
-      console.log(string);
-    }, 1);
-    return string;
-  })
+//   function decrement() {
+//     count--;
+//     console.log(count);
+//   }
 
-  .then((string) => {
-    console.log(
-      "Last Then: oops... didn't bother to instantiate and return a promise in the prior then so the sequence may be a bit surprising"
-    );
+//   return {
+//     increment,
+//     decrement,
+//   };
+// }
 
-    console.log("last",string);
-  });
+// const counter = createCounter();
+// counter.increment(); // 1
 
-  console.log("good to go")
+// counter.increment(); //2
+// counter.decrement(); //-1
+// obj={res:counter}
+
+let timer = 0;
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+  setInterval(() => {
+    timer = timer + 1;
+    console.log("timer inside btn1", timer);
+  }, 2000);
+});
+
+const btn2 = document.getElementById("btn2");
+
+btn.addEventListener("mouseenter", () => {
+  timer = timer + 1;
+  console.log("timer inside btn2", timer);
+});
+
+console.log("timer outside", timer);
--- firstFile.js
+++ firstFile.js
@@ -1,10 +1,24 @@
-function sample (){
+//mXn ==>
 
-  for (let i=0; i<=5;i++){
-  
-  setTimeout (() => console.log(i), 2000 );
-  
+let str = "";
+function convertToString(arr, noOfRow, noOfCol) {
+  for (col = 0; col < noOfCol; col++) {
+    if (col % 2 === 0) {
+      for (row = 0; row < noOfRow; row++) {
+        str += " " + arr[row][col];
+      }
+    } else {
+      for (row = noOfRow - 1; row >= 0; row--) {
+        str += " " + arr[row][col];
+      }
+    }
   }
-  
-  }
-  sample()
+  return str;
+}
+let arr = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16],
+];
+console.log(convertToString(arr, 4, 4));
