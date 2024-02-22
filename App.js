7b48f697ff244709510782fc66adbf9ab96b4741
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-02-22 09:46:19 +0530

    rest operator example

--- App.js
+++ App.js
@@ -1,23 +1,6 @@
-
-//bind
-let obj = {
-  name: "Iphone",
-  price: 100000,
-  rating: 4.3,
-};
-function showName() {
-  console.log("my name is",this.name);
-}
-let d = showName.bind(obj);
-d()
-
-//call 
-let obj2={
-  x:10,
-  getX:function(){
-    console.log(this.x)
-  }
+function abc(first, second, ...args) {
+  console.log(args);
+  console.log(args.length);
 }
 
-   let t=obj.getX
-   t.call(obj)
+abc(1, 2, "abc", "def", "ghi");
