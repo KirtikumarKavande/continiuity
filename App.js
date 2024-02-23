e963112c8671108c06cc6290e04f81d718fe1383
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-02-23 10:54:56 +0530

    reshape matrix

--- App.js
+++ App.js
@@ -1,6 +1,30 @@
-function abc(first, second, ...args) {
-  console.log(args);
-  console.log(args.length);
-}
+/**
+ * @param {number[][]} mat
+ * @param {number} r
+ * @param {number} c
+ * @return {number[][]}
+ */
+var matrixReshape = function (mat, r, c) {
+  m = mat.length;
+  n = mat[0].length;
+  let row = 0;
+  let col = 0;
+  if (m * n !== r * c) return mat;
+  //below is correct implimentaion of black array with given mXn
+  let result = Array(r).fill(0);
+  for (let i = 0; i < r; i++) {
+    result[i] = Array(c).fill(0);
+  }
 
-abc(1, 2, "abc", "def", "ghi");
+  for (let i = 0; i < mat.length; i++) {
+    for (j = 0; j < mat[i].length; j++) {
+      result[row][col] = mat[i][j];
+      col++;
+      if (col === c) {
+        row++;
+        col = 0;
+      }
+    }
+  }
+  return result;
+};
