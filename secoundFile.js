da6cbb0b4b1a1ca0ae63c5a2adf2218b7f8b6238
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-04-05 08:42:08 +0530

    super class

--- secoundFile.js
+++ secoundFile.js
@@ -1,18 +1,23 @@
-function timeOut() {
-  return new Promise(function (resolve, reject) {
-    setTimeout(() => {
-      resolve("timeOut");
-    }, 5000);
-  });
+//parent class
+class Product {
+  constructor(category, price) {
+    this.category = category;
+    this.price = price;
+  }
+  display() {
+    console.log("price", this.price);
+  }
 }
 
-timeOut()
-  .then((data) => {
-    const dataFetch = fetch("https://jsonplaceholder.typicode.com/users");
-    return dataFetch
-  })
-  .then((res) => {
-    return res.json()
-  }).then((data) => {
-    console.log("data",data)
-  })
+const d = new Product();
+
+//extended class
+class mobile extends Product {
+  constructor(color, cat, rs) {
+    super(cat, rs);
+    this.color = color;
+  }
+}
+
+const iphone = new mobile("black", "mobile", "10000");
+iphone.display();
