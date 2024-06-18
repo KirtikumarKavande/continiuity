b79e24de9199d8a0e73349c25478f6804efcd54c
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-06-18 09:49:37 +0530

    count vowels recursively

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-let str = "kirtikumar";
+// let str = "kirtikumar";
 
-let reversedString = "";
-function reverseString(str) {
-  if (str.length === 0) {
-    return str;
+// let count = 0;
+// function countVowels(str) {
+//   console.log("rendered")
+//   const lastChar = str[str.length - 1]
+//   if (lastChar === "a" || lastChar === "e" || lastChar === "i" || lastChar === "o" || lastChar === "u") {
+//     count++;
+//   }
+//   if (str.length === 0) return;
+
+//   countVowels(str.substring(0, str.length - 1));
+// }
+
+// countVowels(str)
+// console.log(count)
+
+let str = "hello";
+
+function isVowel(character) {
+  let vowels = "aeiou";
+  if (vowels.includes(character)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function countVowels(str) {
+  if (str.length === 1) {
+    return Number(isVowel(str[0]));
   }
 
-  return str[str.length - 1] + reverseString(str.substring(0, str.length - 1));
+  return countVowels(str.substring(0,str.length-1))+isVowel(str[str.length-1])
 }
-console.log(reverseString(str))
+
+console.log(countVowels(str))
