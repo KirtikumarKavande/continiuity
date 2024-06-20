e0f34a8f4907e849e2a6fb43aaec3e6b3c7345ce
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-06-20 08:47:04 +0530

    calculating power recursively

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
-// function findFibonacci(num) {
-//   if (num === 0) return 0;
-//   if (num === 1) return 1;
-
-//   return findFibonacci(num - 1) + findFibonacci(num - 2);
+// function countPowResult(num, pow) {
+//   let count=1
+//  return calculatingPower(num,pow,count)
 // }
-// console.log(findFibonacci(10));
 
+// function calculatingPower(num,pow,count){
+//   if (pow === count) {
+//     return num;
+//   }
 
-function findFibonacciIteratively(num) {
-  let n0 = 0;
-  let n1 = 1;
-  let temp = 0;
-  for (let i = 0; i < num; i++) {
-    temp = n0 + n1;
-    n0 = n1;
-    n1 = temp;
-  }
-  return n0;
+//   return calculatingPower(num, pow,count+1) * num;
+// }
+
+// console.log(countPowResult(4, 3));
+
+function countPower(num,pow){
+
+  if(pow===0) return 1
+  return countPower(num,pow-1)*num
+
 }
-
-console.log(findFibonacciIteratively(5));
+console.log(countPower(4,3)
+)
