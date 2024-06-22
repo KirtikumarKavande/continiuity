94f6e2b596278f5f6038070165e487a407ddf445
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-06-22 09:02:39 +0530

    length without using length

--- App.js
+++ App.js
@@ -1,53 +1,7 @@
-// function removeAdjustDuplicate(str) {
-//   let result = "";
-//   for (let i = 0; i < str.length; i++) {
-//     if (!str[i + 1] || str[i] !== str[i + 1]) {
-//       result += str[i];
-//     }
-//   }
-//   return result;
-// }
+const findLengthOfStringRecursively = (str) => {
+  if (str.length === 0) return 0;
 
-// function removeAdjustDuplicateRecursively(str) {
-//   if (str.length === 1) return str;
-
-//   let firstLetter = str[0];
-//   let restOfString = str.slice(1);
-
-//   if (firstLetter === restOfString[0])
-//     return removeAdjustDuplicateRecursively(restOfString);
-
-//   return firstLetter + removeAdjustDuplicateRecursively(restOfString);
-// }
-
-// let str = "aaaaabbbbbdbbccccccccccccc";
-// console.log(removeAdjustDuplicateRecursively(str));
-
-function removeAllConsecutiveDuplicates(str) {
-  let stack = [];
-  return removeAdjustDuplicateRecursively(str, stack);
-}
-
-function removeAdjustDuplicateRecursively(str, stack) {
-  if (str.length === 0 && str !== stack[stack.length - 1]) return str;
-  if (str.length === 0) return "";
-
-  let firstLetter = str[0];
-  let restOfString = str.slice(1);
-
-  if (firstLetter === restOfString[0]) {
-    stack.push(firstLetter);
-
-    return removeAdjustDuplicateRecursively(restOfString, stack);
-  }
-  if (firstLetter === stack[stack.length - 1]) {
-    stack=[]
-    return removeAdjustDuplicateRecursively(restOfString, stack);
-  }
-  console.log(stack)
-
-  return firstLetter + removeAdjustDuplicateRecursively(restOfString, stack);
-}
-let str = "a";
-
-console.log(removeAllConsecutiveDuplicates(str));
+  return findLengthOfStringRecursively(str.substring(1)) + 1;
+};
+let str = "kirtikumar";
+console.log(findLengthOfStringRecursively(str));
