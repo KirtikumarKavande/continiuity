29866390b52ab0a0388278023df52330b92c5a48
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-06-22 09:38:45 +0530

    find number of key occurrence in array

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-const isPalindrome = (str) => {
-  if (str.length <= 1) return true;
+// const findNoOfKeyOccurrence = (array,key) => {
+//   if(array.length===0) return 0
+//   if(key===array.pop()){
+//     return findNoOfKeyOccurrence(array,key)+1
+//   }
 
-  if (str[0] === str[str.length - 1]) {
-   return isPalindrome(str.substring(1, str.length - 1));
-  }else{
-    return false
-  }
+//   return findNoOfKeyOccurrence(array,key)
+
+// };
+// let key =3
+// let array = [2,3,5,7,2,2,8,3,8,3,9,7,4,2];
+// console.log(findNoOfKeyOccurrence(array,key));
+const findNoOfKeyOccurrence = (array, key, index = 0) => {
+  if (index >= array.length) return 0;
+  // return array[index] === key
+    // ? findNoOfKeyOccurrence(array, key,index+1) + 1
+  //   : findNoOfKeyOccurrence(array, key,index+1) + 0;
+  return (array[index] === key
+    ? 1:0 )+findNoOfKeyOccurrence(array, key, index + 1);
 };
-let str = "madam";
-console.log(isPalindrome(str));
+let key = 3;
+let array = [2, 3, 5, 7, 2, 2, 8, 3, 8, 3, 9, 7, 4, 2];
+console.log(findNoOfKeyOccurrence(array, key));
