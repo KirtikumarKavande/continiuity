d7d34c8b7ea5b93795b7e32a14f03918818097b3
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-06-23 23:04:02 +0530

    balance parenthesis problem

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
-// const findNoOfKeyOccurrence = (array,key) => {
-//   if(array.length===0) return 0
-//   if(key===array.pop()){
-//     return findNoOfKeyOccurrence(array,key)+1
-//   }
+function balanceParametersOfSameType(array, startingIndex = 0, count = 0) {
+  if (startingIndex === array.length && count === 0) return true;
+  if (count < 0) return false;
 
-//   return findNoOfKeyOccurrence(array,key)
-
-// };
-// let key =3
-// let array = [2,3,5,7,2,2,8,3,8,3,9,7,4,2];
-// console.log(findNoOfKeyOccurrence(array,key));
-const findNoOfKeyOccurrence = (array, key, index = 0) => {
-  if (index >= array.length) return 0;
-  // return array[index] === key
-    // ? findNoOfKeyOccurrence(array, key,index+1) + 1
-  //   : findNoOfKeyOccurrence(array, key,index+1) + 0;
-  return (array[index] === key
-    ? 1:0 )+findNoOfKeyOccurrence(array, key, index + 1);
-};
-let key = 3;
-let array = [2, 3, 5, 7, 2, 2, 8, 3, 8, 3, 9, 7, 4, 2];
-console.log(findNoOfKeyOccurrence(array, key));
+  if (array[startingIndex] === "(") {
+    return balanceParametersOfSameType(array, startingIndex + 1, count + 1);
+  } else if (array[startingIndex] === ")") {
+    return balanceParametersOfSameType(array, startingIndex + 1, count - 1);
+  }
+  return false
+}
+let array = ["(", "(", ")", ")", "(", ")"];
+console.log(balanceParametersOfSameType(array));
