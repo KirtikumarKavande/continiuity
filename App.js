60637830655d431228efe5247757d45d38e97a7b
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-06-26 10:59:34 +0530

    getting destination node in graph

--- App.js
+++ App.js
@@ -6,18 +6,66 @@ const graph = {
   e: ["b"],
 };
 
-function breadthFirstSearch(graph, start) {
-  const queue = [start];
-  const nodeVisited = new Set([start]);  
-  while (queue.length > 0) {
-    const node = queue.shift();
-    for (const neighbor of graph[node]) {
-      if (!nodeVisited.has(neighbor)) {
-        nodeVisited.add(neighbor);
-        queue.push(neighbor);
-      }
+// function findingDestinationNodeByDFS(graph, start, dest) {
+//   const stack = [start];
+//   const visitedNode = new Set();
+
+//   while (stack.length > 0) {
+//     const node = stack.pop();
+//     if (node == dest) return true;
+//     if (!visitedNode.has(node)) {
+//       visitedNode.add(node);
+//       for (const neighbor of graph[node]) {
+//         stack.push(neighbor);
+//       }
+//     }
+//   }
+//   return false;
+// }
+// console.log(findingDestinationNodeByDFS(graph, "a", "e"));
+
+// //by breadth first
+
+// function findingDestinationNodeByBFS(graph, start, dest) {
+//   const queue = [start];
+//   const nodeVisited = new Set();
+
+//   while (queue.length > 0) {
+//     const node = queue.shift();
+//     if (node == dest) {
+//       return true;
+//     }
+
+//     for (const neighbor of graph[node]) {
+//       if (!nodeVisited.has(neighbor)) {
+//         nodeVisited.add(node)
+//         queue.push(neighbor);
+//       }
+//     }
+//   }
+//   return false
+// }
+
+// console.log(findingDestinationNodeByBFS(graph,"a","e"))
+
+function findingDestinationNodeByRecursively(
+  graph,
+  start,
+  dest,
+  visited = new Set()
+) {
+  if (start === dest) return true;
+  if (visited.has(start)) return false;
+
+  visited.add(start);
+
+  for (const neighbor of graph[start]) {
+    if (findingDestinationNodeByRecursively(graph, neighbor, dest, visited)) {
+      return true;
     }
   }
-  return nodeVisited;
+
+  return false;
 }
-console.log(breadthFirstSearch(graph, "a"));
+
+console.log(findingDestinationNodeByRecursively(graph, "a", "efff"));
