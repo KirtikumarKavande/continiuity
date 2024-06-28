6de984151c610a051f3bcbbf8ea85d716a92f315
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-06-28 08:44:22 +0530

    converting undirected graph to directed graph

--- App.js
+++ App.js
@@ -1,71 +1,39 @@
-const graph = {
-  a: ["b", "c"],
-  b: ["c", "d"],
-  c: ["d", "e"],
-  d: [],
-  e: ["b"],
-};
+const edges = [
+  ["b", "a"],
+  ["c", "a"],
+  ["b", "c"],
+];
+function createGraph(edges) {
+  let graph = {};
+  for (const edge of edges) {
+    const [x, y] = edge;
+    if (!(x in graph)) {
+      graph[x] = [];
+    }
+    if (!(y in graph)) {
+      graph[y] = [];
+    }
 
-// function findingDestinationNodeByDFS(graph, start, dest) {
-//   const stack = [start];
-//   const visitedNode = new Set();
+    graph[x].push(y);
+    graph[y].push(x);
+  }
+  return graph;
+}
+const graph = createGraph(edges);
 
-//   while (stack.length > 0) {
-//     const node = stack.pop();
-//     if (node == dest) return true;
-//     if (!visitedNode.has(node)) {
-//       visitedNode.add(node);
-//       for (const neighbor of graph[node]) {
-//         stack.push(neighbor);
-//       }
-//     }
-//   }
-//   return false;
-// }
-// console.log(findingDestinationNodeByDFS(graph, "a", "e"));
+function DFSForGraph(graph, start, dest, visitedNode = new Set()) {
+  const stack = [start];
+  while (stack.length !== 0) {
+    const node = stack.pop();
+    if (visitedNode.has(node)) return false;
 
-// //by breadth first
-
-// function findingDestinationNodeByBFS(graph, start, dest) {
-//   const queue = [start];
-//   const nodeVisited = new Set();
-
-//   while (queue.length > 0) {
-//     const node = queue.shift();
-//     if (node == dest) {
-//       return true;
-//     }
-
-//     for (const neighbor of graph[node]) {
-//       if (!nodeVisited.has(neighbor)) {
-//         nodeVisited.add(node)
-//         queue.push(neighbor);
-//       }
-//     }
-//   }
-//   return false
-// }
-
-// console.log(findingDestinationNodeByBFS(graph,"a","e"))
-
-function findingDestinationNodeByRecursively(
-  graph,
-  start,
-  dest,
-  visited = new Set()
-) {
-  if (start === dest) return true;
-  if (visited.has(start)) return false;
-
-  visited.add(start);
-
-  for (const neighbor of graph[start]) {
-    if (findingDestinationNodeByRecursively(graph, neighbor, dest, visited)) {
-      return true;
+    visitedNode.add(node);
+    if (node === dest) return true;
+    for (const ele of graph[node]) {
+      stack.push(ele);
     }
   }
-
   return false;
 }
 
-console.log(findingDestinationNodeByRecursively(graph, "a", "efff"));
+console.log(DFSForGraph(graph, "a", "c"));
