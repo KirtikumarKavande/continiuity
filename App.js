cecb623c33592a091ac9edb031263082ff054cd7
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-07-04 14:00:23 +0530

    merge alternatively string

--- App.js
+++ App.js
@@ -1,43 +1,21 @@
-const grid = [
-  ["L", "W", "W", "L", "W"],
-  ["L", "W", "W", "L", "L"],
-  ["W", "L", "L", "W", "W"],
-  ["W", "W", "W", "W", "W"],
-  ["W", "W", "L", "L", "L"],
-];
-function findingLand(grid) {
-  const rowLength = grid.length;
-  const colLength = grid[0].length;
-  const visited = new Set();
-  let count = 0;
-  function dfs(r, c) {
-    if (
-      r < 0 ||
-      r >= rowLength ||
-      c < 0 ||
-      c >= colLength ||
-      visited.has(r + "," + c) ||
-      grid[r][c] === "W"
-    ) {
-      return;
-    }
-    visited.add(r + "," + c);
-    dfs(r - 1, c);
-    dfs(r + 1, c);
-    dfs(r, c - 1);
-    dfs(r, c + 1);
+var mergeAlternately = function(word1, word2) {
+  let i=0
+  let j=0
+  let str=""
+  while(word1[i]&& word2[j]){
+      str+=word1[i]
+      str+=word2[i]
+      i++
+      j++
   }
+      while(i<word1.length){
 
-  for (let r = 0; r < rowLength; r++) {
-    for (let c = 0; c < colLength; c++) {
-      if (grid[r][c] === "L") {
-        if (!visited.has(r + "," + c)) {
-          count++;
-          dfs(r, c);
-        }
+          str+=word1[i]
+          i++
       }
-    }
-  }
-  return count;
-}
-console.log(findingLand(grid));
+      while(j<word2.length){
+          str+=word2[j]
+          j++
+      }
+  return str
+};
