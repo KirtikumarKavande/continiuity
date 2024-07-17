368860d80601256e041b20d85f3525ae43f1f4a8
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-07-17 21:20:05 +0530

    longest subarray

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-var longestOnes = function (nums, k) {
-  let left = 0;
-  let maxCount = 0;
-  let zerosCounter = 0;
-  for (let right = 0; right < nums.length; right++) {
-    if (nums[right] === 0) zerosCounter++;
-    while (zerosCounter > k) {
-      if (nums[left] === 0) {
-        zerosCounter--;
+var longestSubarray = function(nums) {
+  let left=0
+  let maxOnes=0
+  let zeros=0
+  for(let right=0;right<nums.length;right++){
+      if(nums[right]===0){
+          zeros++
       }
-      left++;
-    }
-    if (right - left + 1 > maxCount) {
-      maxCount = right - left + 1;
-    }
+      while(zeros>1){
+          if(nums[left]===0){
+              zeros--
+          }
+          left++
+      }
+      if(right-left>maxOnes){
+          maxOnes=right-left
+      }
   }
-  return maxCount;
+  return maxOnes
 };
