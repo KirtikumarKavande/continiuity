ca81d9f75401335c0f8796b5f63e0f0f90fd7dfc
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-09-21 10:36:58 +0530

    isSequence

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-var gcdOfStrings = function(str1, str2) {
-    let i=1
- if(str1+str2!==str2+str1) return ""
-    let maxNumber=Math.min(str1.length,str2.length)
-    let gcd=null
-    for (i=0;i<=maxNumber;i++){
-        if(str1.length%i===0 && str2.length%i===0 ){
-            gcd=i
+var isSubsequence = function(s, t) {
+    let i=0
+    let j=0
+    while(i<s.length && j<t.length){
+        if(s[i]===t[j]){
+            i++
+            j++
+        }else{
+            j++
         }
     }
-       let sub1=str1.substring(0,gcd)
-       let sub2=str2.substring(0,gcd)
-       if(sub1===sub2){
-        return sub1
-       }else{
-        while(sub1!==sub2){
-             sub1=str1.substring(0,gcd--)
-             sub2=str2.substring(0,gcd--)
-        }
-  
-        return str1.substring(0,gcd)
-
-       
-       }
+    if(i===s.length){
+        return true
+    }
+    return false
 };
