5f54d055326a287745d39fdefda9356bd0ab787e
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-10-02 16:26:09 +0530

    equal pairs problem

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-Array.prototype.customFilter=function(cb){
-let arr=[]
-    for (let index = 0; index < this.length; index++) {
-         let result= cb(this[index],index)
-         if(result){
-            arr.push(this[index])
-         }
-        
+const equalPairs = function(grid) {
+    let rowMap = new Map();
+    let colMap = new Map();
+    let count = 0;
+
+    // Count occurrences of each row as strings in rowMap
+    for (let row = 0; row < grid.length; row++) {
+        let rowStr = grid[row].join('+');
+        rowMap.set(rowStr, (rowMap.get(rowStr) || 0) + 1);
     }
 
-    return arr
+    // Count occurrences of each column as strings in colMap
+    for (let col = 0; col < grid[0].length; col++) {
+        let colStr = grid.map(row => row[col]).join('+');
+        // let colStr = grid.map(row => {
+        //     console.log(row)
+        //     return row[col]})
 
-}
 
-let res=[1,2,3,4,5].customFilter((item)=>{
-    return item/2
-})
-console.log(res)
+        colMap.set(colStr, (colMap.get(colStr) || 0) + 1);
+    }
 
-let res2=[1,2,3,4,5].filter((item)=>{
-    return item/2
-})
-console.log(res2)
+    // Count pairs where rows and columns match
+    for (let [rowStr, rowCnt] of rowMap.entries()) {
+        if (colMap.has(rowStr)) {
+            count += rowCnt * colMap.get(rowStr);
+        }
+    }
+
+    return count;
+};
+
+let grid=[[3,2,1],[1,7,6],[2,7,7]]
+equalPairs(grid)
