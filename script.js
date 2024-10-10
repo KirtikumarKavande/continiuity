7a381da2b84bd3a4f732679ef2acff765a50034f
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-10-10 08:15:36 +0530

    implementation of promise over callback

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-const equalPairs = function(grid) {
-    let rowMap = new Map();
-    let colMap = new Map();
-    let count = 0;
+// cb
+function download(url,cb) {
+    
+    setTimeout(()=>{
+        cb("hello world"+url)
+        cb("hello world"+url)
+        cb("hello world"+url)
+    },2000)
+}
 
-    // Count occurrences of each row as strings in rowMap
-    for (let row = 0; row < grid.length; row++) {
-        let rowStr = grid[row].join('+');
-        rowMap.set(rowStr, (rowMap.get(rowStr) || 0) + 1);
-    }
+download("https://www.google.com",function(data){
 
-    // Count occurrences of each column as strings in colMap
-    for (let col = 0; col < grid[0].length; col++) {
-        let colStr = grid.map(row => row[col]).join('+');
-        // let colStr = grid.map(row => {
-        //     console.log(row)
-        //     return row[col]})
+    console.log("downloaded",data)
+})
+
+// promise
+
+function fetachData(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve("url")
+            resolve("url")
+            resolve("url")
+        },4000)
+    })
+}
 
 
-        colMap.set(colStr, (colMap.get(colStr) || 0) + 1);
-    }
-
-    // Count pairs where rows and columns match
-    for (let [rowStr, rowCnt] of rowMap.entries()) {
-        if (colMap.has(rowStr)) {
-            count += rowCnt * colMap.get(rowStr);
-        }
-    }
-
-    return count;
-};
-
-let grid=[[3,2,1],[1,7,6],[2,7,7]]
-equalPairs(grid)
+fetachData().then((data)=>{
+console.log("dataa",data)
+})
