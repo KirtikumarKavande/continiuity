2d5aca2ec097bab81292694bd7e7e1eb8b5d6a65
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-01-01 18:11:13 +0530

    chaning  example

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-function Parent(name,age) {
-    this.name=name;
-    this.age=age
-    
-}
-
-
-
-function Child(name,age,clg) {
-    this.clg=clg
-    Parent.call(this,name,age)
-
-    this.printDetails=function(){
-      console.log(this.name,this.age, this.clg)  
-    }
-    
-}
-
-let data= new Child("kirti",10,"GCEK")
-data.printDetails()
+let child1=document.getElementById("child1")
+child1.addEventListener('drop',()=>{
+    console.log("hi")
+})
