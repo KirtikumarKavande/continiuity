a1825e0168835e9b4ce4b043438a3d7cef07fc02
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-02-22 22:10:29 +0530

    implement splitBunches function to distribute quantities into individual items

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-var guessNumber = function(n) {
-    let i=1
-   let j=n
-   for (let index = 0; index <=n; index++) {
-       let mid=Math.floor((i+j)/2) 
-          let result= guess(mid)
-       if(result===0){
-           return mid
-       }else if(result===1){
-           i=mid+1
+const splitBunches = (arr) => {
+    let result=[]
+      function placeItem(obj){
+        if(obj.quantity!==1){
+          result.push({...obj,quantity:1})
+          obj.quantity=obj.quantity-1
+          placeItem(obj)
+        }else {
+          result.push({...obj,quantity:1})
+          
+        }
+      }
+      for(let element of arr){
+        placeItem(element)
+      }
+    
+      return result
+    };
+    let ans=splitBunches([{name:"a",quantity:2},{name:"b",quantity:1},{name:"c",quantity:3}])
 
-       }else{
-           j=mid-1
-       }
-       
-   } 
-};
+
+    console.log(ans)
